5eea11994c118716b5546b59e868e7bcb518075a
Simon Willison <swillison@gmail.com>
2021-11-02 16:35:05 -0700

    Policy templates, refs #3

---
+++ s3_credentials/policies.py
@@ -0,0 +1,48 @@
+def read_write(bucket):
+    # https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_s3_rw-bucket.html
+    return {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "ListObjectsInBucket",
+                "Effect": "Allow",
+                "Action": ["s3:ListBucket"],
+                "Resource": ["arn:aws:s3:::{}".format(bucket)],
+            },
+            {
+                "Sid": "AllObjectActions",
+                "Effect": "Allow",
+                "Action": "s3:*Object",
+                "Resource": ["arn:aws:s3:::{}/*".format(bucket)],
+            },
+        ],
+    }
+
+
+def read_only(bucket):
+    return {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": ["s3:GetObject*", "s3:ListBucket"],
+                "Resource": [
+                    "arn:aws:s3:::{}".format(bucket),
+                    "arn:aws:s3:::{}/*".format(bucket),
+                ],
+            }
+        ],
+    }
+
+
+def write_only(bucket):
+    return {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": ["s3:PutObject"],
+                "Resource": ["arn:aws:s3:::{}/*".format(bucket)],
+            }
+        ],
+    }
