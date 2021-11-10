c6d8766d1d9caf67e20dcfc254b03a28dbad8287
Simon Willison <swillison@gmail.com>
2021-11-09 21:15:11 -0800

    Redesigned read only policy, closes #23, refs #15

--- s3_credentials/policies.py
+++ s3_credentials/policies.py
@@ -18,21 +18,29 @@ def read_write(bucket):
 
 
 def read_only(bucket):
-    return {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": ["s3:ListBucket"],
-                "Resource": ["arn:aws:s3:::{}".format(bucket)],
-            },
-            {
-                "Effect": "Allow",
-                "Action": "s3:GetObject*",
-                "Resource": ["arn:aws:s3:::{}/*".format(bucket)],
-            },
-        ],
-    }
+    return {"Version": "2012-10-17", "Statement": read_only_statements(bucket)}
+
+
+def read_only_statements(bucket):
+    # https://github.com/simonw/s3-credentials/issues/23
+    return [
+        {
+            "Effect": "Allow",
+            "Action": ["s3:ListBucket"],
+            "Resource": ["arn:aws:s3:::{}".format(bucket)],
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectAcl",
+                "s3:GetObjectLegalHold",
+                "s3:GetObjectRetention",
+                "s3:GetObjectTagging",
+            ],
+            "Resource": ["arn:aws:s3:::{}/*".format(bucket)],
+        },
+    ]
 
 
 def write_only(bucket):
